d47119d3ac4cf91179333d7828913fe7a31b6074
Sonal <sonal.saldanha@gmail.com>
2014-10-23 01:42:31 +0530

    Initial Commit
    Added the files. Ready to export.

---
+++ summarizer.py
@@ -0,0 +1,50 @@
+import re, requests, sys
+from bs4 import BeautifulSoup, NavigableString
+import bleach
+import summary
+
+t = " "
+
+def strip_tags(data,tags):
+	pattern = ['&lt','&gt','/[.*/]','html','body',';',':','/']
+	soup = BeautifulSoup(data)
+	
+	for tag in soup.findAll(True):
+		if tag.name in tag:
+			s = ""
+			for c in tag.contents:
+				if not isinstance(c, NavigableString):
+					c = strip_tags(unicode(c), tag)
+				s +=unicode(c)
+			tag.replaceWith(s)
+
+	p = soup.findAll('p')
+
+	for para in p:
+		p1 = bleach.clean(para, strip = True)
+		p2 = re.sub(pattern[0],"",p1)	
+
+		for pat in pattern:
+			p2 = re.sub(pat,"",p2)
+			build_text(p2)
+
+
+def build_text(txt):
+	global t  
+	t = t + " " + txt 
+
+
+def summarize(url):
+	global t
+	r = requests.get(url)
+	data= r.text
+	tags = ['a','span','h2','img','sup','i','b']
+	# soup = BeautifulSoup(data)
+	strip_tags(data,tags)
+	return summary.summary(t)
+
+
+if __name__ == "__main__":
+	url = raw_input("URL:")
+	url = "http://en.wikipedia.org/wiki/"+url
+	print summarize(url)
